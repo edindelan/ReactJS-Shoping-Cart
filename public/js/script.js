3e884fed061fe06ecb6ab7c65c8472ceea6a429a
del4n <edindelanovic@hotmail.com>
2016-10-09 23:58:15 +0200

    Create product form

--- public/js/script.js
+++ public/js/script.js
@@ -37,32 +37,78 @@ var Total = React.createClass({
     }
 });
 
+var ProductForm = React.createClass({
+    submit: function (e) {
+      e.preventDefault();
+
+        var product = {
+            name: this.refs.name.value,
+            price: parseInt(this.refs.price.value)
+        }
+
+        this.props.handleCreateProduct(product);
+
+        this.refs.name.value = "";
+        this.refs.price.value = "";
+    },
+   render: function () {
+       return (
+           <form>
+               <input type="text" placeholder="Product Name" ref="name"/> -
+               <input type="text" placeholder="Product Price" ref="price"/>
+               <br/>
+               <br/>
+               <button onClick={this.submit}>Create Product</button>
+               <hr/>
+           </form>
+       )
+   }
+});
+
 var ProductList = React.createClass({
     getInitialState: function () {
         return {
-            total: 0
+            total: 0,
+            productList: [{
+                    name: "Android",
+                    price: 121
+                },
+                {
+                    name: "Apple",
+                    price: 650
+                },
+                {
+                    name: "Nokia",
+                    price: 254
+                }]
         }
     },
+    createProduct: function (product) {
+        this.setState({
+            productList: this.state.productList.concat(product)
+        })
+    },
+
     calculateTotal: function (price) {
       this.setState({total: this.state.total + price});
         console.log(price, this.state.total);
-        alert(this.state.total);
     },
     showProduct: function (name) {
         alert("You selected " + name)
     },
     render: function () {
+        var products = this.state.productList.map(function (product) {
+            return (
+                <Product name={product.name} price={product.price}
+                handleShow={this.showProduct}
+                handleTotal={this.calculateTotal}/>
+                );
+
+        }.bind(this));
         return (
             <div>
-                <Product name="Android" price={121}
-                         handleShow={this.showProduct}
-                         handleTotal={this.calculateTotal}/>
-                <Product name="Apple" price={65}
-                         handleShow={this.showProduct}
-                         handleTotal={this.calculateTotal}/>
-                <Product name="Nokia" price={254}
-                         handleShow={this.showProduct}
-                         handleTotal={this.calculateTotal}/>
+                <ProductForm handleCreateProduct={this.createProduct}/>
+                {products}
                 <Total total={this.state.total}/>
             </div>
         )
